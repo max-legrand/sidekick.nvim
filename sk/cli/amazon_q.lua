4b65b8ad7e5e7e91e3db24a9061c6eb561fe2fd4
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-10-08 16:27:23 +0200

    fix(q): filer procs on qchat instead of `q`, which seems to be the actual binary. Closes #80

--- sk/cli/amazon_q.lua
+++ sk/cli/amazon_q.lua
@@ -1,6 +1,6 @@
 ---@type sidekick.cli.Config
 return {
   cmd = { "q" },
-  is_proc = "\\<q\\>",
-  url = "https://github.com/aws/amazon-q-developer-cli"
+  is_proc = "\\<qchat\\>",
+  url = "https://github.com/aws/amazon-q-developer-cli",
 }
