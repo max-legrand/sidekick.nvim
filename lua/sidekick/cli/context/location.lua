d570e1f83bf2a19e6c1fad5de9b1b07beb54b67d
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-10-09 20:34:42 +0200

    fix(location): different format that should work for most cli tools. Closes #59

--- lua/sidekick/cli/context/location.lua
+++ lua/sidekick/cli/context/location.lua
@@ -18,38 +18,53 @@ function M.get(ctx, opts)
     end
   end
 
-  local ret = {} ---@type sidekick.Text
-  if opts.kind == "position" and ctx.range then
-    local from, to = ctx.range.from, ctx.range.to
+  local from = ctx.range and ctx.range.from or { ctx.row, ctx.col }
+  local to = ctx.range and ctx.range.to or nil
+
+  -- normalize order
+  if from and to then
     if from[1] > to[1] or (from[1] == to[1] and from[2] > to[2]) then
       from, to = to, from
     end
-    if ctx.range.kind == "line" and from[1] == to[1] then
-      ret[#ret + 1] = { ("%d"):format(from[1]), "SnacksPickerRow" }
-    elseif ctx.range.kind == "line" then
-      ret[#ret + 1] = { ("%d-%d"):format(from[1], to[1]), "SnacksPickerRow" }
-    elseif from[1] == to[1] then -- block/char on same line
-      ret[#ret + 1] = { ("%d"):format(from[1]), "SnacksPickerRow" }
-      ret[#ret + 1] = { ":", "SnacksPickerDelim" }
-      ret[#ret + 1] = { ("%d-%d"):format(from[2] + 1, to[2] + 1), "SnacksPickerCol" }
-    else -- block/char on different lines
-      ret[#ret + 1] = { ("%d"):format(from[1]), "SnacksPickerRow" }
-      ret[#ret + 1] = { ":", "SnacksPickerDelim" }
-      ret[#ret + 1] = { ("%d-%d:%d"):format(from[2] + 1, to[1], to[2] + 1), "SnacksPickerCol" }
-    end
-  elseif opts.kind == "position" and ctx.row and ctx.col then
-    ret[#ret + 1] = { ("%d"):format(ctx.row), "SnacksPickerRow" }
-    ret[#ret + 1] = { ":", "SnacksPickerDelim" }
-    ret[#ret + 1] = { ("%d"):format(ctx.col), "SnacksPickerCol" }
-  elseif (opts.kind == "position" or opts.kind == "line") and ctx.row then
-    ret[#ret + 1] = { ("%d"):format(ctx.row), "SnacksPickerRow" }
   end
 
-  if #ret > 0 then
-    table.insert(ret, 1, { ":", "SnacksPickerDelim" })
+  local ret = {} ---@type sidekick.Text
+
+  ---@param ... string|number
+  local function add(...)
+    for _, v in ipairs({ ... }) do
+      if type(v) == "number" then
+        ret[#ret + 1] = { tostring(v), "SidekickLocNum" }
+      elseif v == "L" then
+        ret[#ret + 1] = { v, "SidekickLocRow" }
+      elseif v == "C" then
+        ret[#ret + 1] = { v, "SidekickLocCol" }
+      else
+        ret[#ret + 1] = { v, "SidekickLocDelim" }
+      end
+    end
   end
-  table.insert(ret, 1, { name, "SnacksPickerDir" })
-  table.insert(ret, 1, { "@", "Bold" })
+
+  add("@")
+  ret[#ret + 1] = { name, "SidekickLocFile" }
+
+  if opts.kind == "line" or (ctx.range and ctx.range.kind == "line") then
+    add(" ")
+    add(":", "L", from[1])
+    if to and from[1] ~= to[1] then
+      add("-", "L", to[1])
+    end
+  elseif opts.kind == "position" then
+    add(" ")
+    if to and from[1] == to[1] and from[2] ~= to[2] then
+      add(":", "L", from[1], ":", "C", from[2] + 1, "-", "C", to[2] + 1)
+    elseif to and from[2] ~= to[2] then
+      add(":", "L", from[1], ":", "C", from[2] + 1, "-", "L", to[1], ":", "C", to[2] + 1)
+    else
+      add(":", "L", from[1], ":", "C", from[2] + 1)
+    end
+  end
+
   return { ret }
 end
 
