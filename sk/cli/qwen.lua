c262b25be46de67b96ca38c005a97d7091025818
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-10-11 12:13:28 +0200

    fix(qwen): set `mux_focus = true` since qwen doesnt process input if unfocused. Fixes #104

--- sk/cli/qwen.lua
+++ sk/cli/qwen.lua
@@ -2,5 +2,6 @@
 return {
   cmd = { "qwen" },
   is_proc = "\\<qwen\\>",
-  url = "https://github.com/QwenLM/qwen-code"
+  url = "https://github.com/QwenLM/qwen-code",
+  mux_focus = true, -- qwen needs focus in order to process input
 }
